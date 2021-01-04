7cc8514a8fc375b4f0e65422715984278c589a92
Valiantsin Ulasau <valiantsin.ulasau@dkfz-heidelberg.de>
2021-01-04 09:44:41 +0100

    Modifying install/uninstall scripts to deal with unite, unite-donors-feed and unite-mutations-feed applications.

--- scripts/install.dev.sh
+++ scripts/install.dev.sh
@@ -29,7 +29,25 @@ cd ../../scripts
 echo ""
 echo ""
 
+cd ../applications/unite
+sh install.dev.sh
+cd ../../scripts
+echo ""
+echo ""
+
 cd ../applications/unite-composer
+sh install.dev.sh
+cd ../../scripts
+echo ""
+echo ""
+
+cd ../applications/unite-donors-feed
+sh install.dev.sh
+cd ../../scripts
+echo ""
+echo ""
+
+cd ../applications/unite-mutations-feed
 sh install.dev.sh
 cd ../../scripts
 echo ""
--- scripts/install.local.sh
+++ scripts/install.local.sh
@@ -29,7 +29,25 @@ cd ../../scripts
 echo ""
 echo ""
 
+cd ../applications/unite
+sh install.local.sh
+cd ../../scripts
+echo ""
+echo ""
+
 cd ../applications/unite-composer
+sh install.local.sh
+cd ../../scripts
+echo ""
+echo ""
+
+cd ../applications/unite-donors-feed
+sh install.local.sh
+cd ../../scripts
+echo ""
+echo ""
+
+cd ../applications/unite-mutations-feed
 sh install.local.sh
 cd ../../scripts
 echo ""

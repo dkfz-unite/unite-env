5427379bb7f622ef1e6e140b2bc0d2a2586c6cf6
Valiantsin Ulasau <valiantsin.ulasau@dkfz-heidelberg.de>
2021-01-05 13:02:41 +0100

    Changes to configuration file.

--- scripts/configure.linux.sh
+++ scripts/configure.linux.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+sh configure.sh
+echo ""
+
 tput setaf 6; echo "# Setting up kernel setting 'vm.max_map_count' to minimal required value of '262144'"; tput sgr0
 sysctl -w vm.max_map_count=262144

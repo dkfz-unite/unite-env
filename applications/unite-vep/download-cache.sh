5f3ddee12923e96e121d323e77ed2bb542e55a98
vulasau <valiantsin.ulasau@gmail.com>
2022-03-03 13:39:45 +0100

    Changed VEP change download scripts.

--- applications/unite-vep/download-cache.sh
+++ applications/unite-vep/download-cache.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-wget -O ./data/homo_sapiens_vep_103_GRCh37.tar.gz http://ftp.ensembl.org/pub/release-103/variation/vep/homo_sapiens_vep_103_GRCh37.tar.gz
+if [ ! -d "./data" ] 
+then
+  mkdir data
+fi
+
+wget -c -O ./data/homo_sapiens_vep_103_GRCh37.tar.gz http://ftp.ensembl.org/pub/release-103/variation/vep/homo_sapiens_vep_103_GRCh37.tar.gz
---
+++ applications/unite-vep/extract-cache.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+tar -xvf ./data/homo_sapiens_vep_103_GRCh37.tar.gz -C ./data

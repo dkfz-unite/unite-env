d155c2aa3894ba0f010b7304fea0beb1de75a7b2
vulasau <valiantsin.ulasau@gmail.com>
2022-04-26 11:53:42 +0200

    Update configure.sh

--- scripts/configure.sh
+++ scripts/configure.sh
@@ -9,5 +9,5 @@ tput setaf 6; echo "# Changing secrets.json permissions to 600"; tput sgr0
 chmod 600 ../secrets.json
 echo ""
 
-tput setaf 6; echo "# Creating docker network 'unite'"; tput sgr0
-docker network create unite -o "com.docker.network.driver.mtu=1450"
+# tput setaf 6; echo "# Creating docker network 'unite'"; tput sgr0
+# docker network create unite -o "com.docker.network.driver.mtu=1450"

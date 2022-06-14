9ff2132c54f3ca1703f91026196933f20d349772
DGitRich <daniel.rich@gmx.net>
2022-06-14 15:21:23 +0200

    Create install.apps.sh script
    Add script to install apps only. Excluding Ensembl VEP wrapper

---
+++ scripts/install.apps.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+cd ../applications/unite-data-migrations
+sh install.sh
+cd ../../scripts
+echo ""
+echo ""
+
+cd ../applications/unite-identity-migrations
+sh install.sh
+cd ../../scripts
+echo ""
+echo ""
+
+cd ../applications/unite
+sh install.sh
+cd ../../scripts
+echo ""
+echo ""
+
+cd ../applications/unite-composer
+sh install.sh
+cd ../../scripts
+echo ""
+echo ""
+
+cd ../applications/unite-donors-feed
+sh install.sh
+cd ../../scripts
+echo ""
+echo ""
+
+cd ../applications/unite-images-feed
+sh install.sh
+cd ../../scripts
+echo ""
+echo ""
+
+cd ../applications/unite-specimens-feed
+sh install.sh
+cd ../../scripts
+echo ""
+echo ""
+
+cd ../applications/unite-genome-feed
+sh install.sh
+cd ../../scripts
+echo ""
+echo ""

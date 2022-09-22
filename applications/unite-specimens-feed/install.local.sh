6d53960fb9c568c9dda7babdb6537337f42d0b67
vulasau <valiantsin.ulasau@gmail.com>
2022-09-22 12:05:38 +0200

    Specimens feed api key protection.

--- applications/unite-specimens-feed/install.local.sh
+++ applications/unite-specimens-feed/install.local.sh
@@ -6,6 +6,7 @@ sql_usr=$(jq -r .sql.user ../../secrets.json)
 sql_pwd=$(jq -r .sql.password ../../secrets.json)
 els_usr=$(jq -r .elasticsearch.user ../../secrets.json)
 els_pwd=$(jq -r .elasticsearch.password ../../secrets.json)
+api_key=$(jq -r .api.key ../../secrets.json)
 src_pth=$(jq -r .SourceCodeDirectoryPath ../../config.json)
 
 tput setaf 6; echo "# Installing UNITE Specimens Datafeed service"; tput sgr0
@@ -27,4 +28,5 @@ ELASTIC_USER=$els_usr \
 ELASTIC_PASSWORD=$els_pwd \
 SQL_USER=$sql_usr \
 SQL_PASSWORD=$sql_pwd \
+API_KEY=$api_key \
 docker-compose -p '' -f docker-compose.yml up -d --build

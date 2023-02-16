658ba405d0b8117cf84a63ca045e60e80a270a54
vulasau <valiantsin.ulasau@gmail.com>
2023-02-16 17:04:22 +0100

    Update download-cache.sh

--- applications/unite-vep/download-cache.sh
+++ applications/unite-vep/download-cache.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+dbr=108
 
 if [ ! -d "./data" ] 
 then
   mkdir data
 fi
 
-wget -c -O ./data/homo_sapiens_vep_103_GRCh37.tar.gz http://ftp.ensembl.org/pub/release-103/variation/vep/homo_sapiens_vep_103_GRCh37.tar.gz
+wget -c -O ./data/homo_sapiens_vep_103_GRCh37.tar.gz http://ftp.ensembl.org/pub/release-103/variation/vep/homo_sapiens_vep_$dbr_GRCh37.tar.gz

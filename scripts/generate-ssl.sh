8d5785c47dc995a090f47ca19bc311022017a5b2
vulasau <valiantsin.ulasau@gmail.com>
2023-03-24 13:28:00 +0100

    Update generate-ssl.sh

--- scripts/generate-ssl.sh
+++ scripts/generate-ssl.sh
@@ -10,12 +10,11 @@ cd ../ssl
 if [ -z "$1" ]
 then
   echo "Generating ssl certificate for localhost"
-  # mkcert -key-file unite-key.pem -cert-file unite-cert.pem localhost 127.0.0.1 ::1
-  openssl req -x509 -newkey rsa:4096 -keyout unite-key.pem -out unite-cert.pem -sha256 -days 365 -nodes -subj "/CN=127.0.0.1,::1,localhost/O=DKFZ/C=DE"
+  # 127.0.0.1,::1,
+  openssl req -x509 -newkey rsa:4096 -keyout unite-key.pem -out unite-cert.pem -sha256 -days 365 -nodes -subj "/CN=localhost"
 else
   echo "Generating ssl certificate for '$1'"
-  openssl req -x509 -newkey rsa:4096 -keyout unite-key.pem -out unite-cert.pem -sha256 -days 365 -nodes -subj "/CN=127.0.0.1,::1,localhost,$1/O=DKFZ/C=DE"
-  # mkcert -key-file unite-key.pem -cert-file unite-cert.pem $1 localhost 127.0.0.1 ::1
+  openssl req -x509 -newkey rsa:4096 -keyout unite-key.pem -out unite-cert.pem -sha256 -days 365 -nodes -subj "/CN=localhost, $1"
 fi
 
 chmod 600 unite-key.pem

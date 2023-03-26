8ff9e5752a1f4035ad098007434df5f1d830de95
vulasau <valiantsin.ulasau@gmail.com>
2023-03-26 16:45:06 +0200

    Update configure.sh

--- scripts/configure.sh
+++ scripts/configure.sh
@@ -1,13 +1,1 @@
 #!/bin/bash
-
-tput setaf 6; echo "# Adding secrets.json to local ignore list"; tput sgr0
-git update-index --assume-unchanged ../secrets.json
-git update-index --skip-worktree ../secrets.json
-echo ""
-
-tput setaf 6; echo "# Changing secrets.json permissions to 600"; tput sgr0
-chmod 600 ../secrets.json
-echo ""
-
-# tput setaf 6; echo "# Creating docker network 'unite'"; tput sgr0
-# docker network create unite -o "com.docker.network.driver.mtu=1450"

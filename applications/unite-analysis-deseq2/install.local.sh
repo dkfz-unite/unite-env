81c8f2d3647298707a82e1b4928241b255ba746d
vulasau <valiantsin.ulasau@gmail.com>
2023-10-13 13:00:05 +0200

    DESeq2 analysis installation files.

---
+++ applications/unite-analysis-deseq2/install.local.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+src_pth=$(jq -r .SourceCodeDirectoryPath ../../config.json)
+
+tput setaf 6; echo "# Installing UNITE analysis service (DESeq2)"; tput sgr0
+echo ""
+
+tput setaf 4; echo "# Cleaning up source code directory"; tput sgr0
+rm -r -f src
+echo ""
+
+tput setaf 4; echo "# Cloning fresh code to source code directory"; tput sgr0
+mkdir src
+cp -r $src_pth/unite-analysis-deseq2/. src/
+echo ""
+
+tput setaf 4; echo "# Building and running docker image"; tput sgr0
+docker-compose -p '' -f docker-compose.local.yml up -d --build
---
+++ applications/unite-analysis-deseq2/install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+ghb_usr=$(jq -r .github.user ../../secrets.json)
+ghb_tkn=$(jq -r .github.token ../../secrets.json)
+
+tput setaf 6; echo "# Installing UNITE analysis service (DESeq2)"; tput sgr0
+echo ""
+
+tput setaf 4; echo "# Cleaning up source code directory"; tput sgr0
+rm -r -f src
+echo ""
+
+tput setaf 4; echo "# Cloning fresh code to source code directory"; tput sgr0
+git clone https://$ghb_tkn@github.com/dkfz-unite/unite-analysis-deseq2.git src
+echo ""
+
+tput setaf 4; echo "# Building and running docker image"; tput sgr0
+docker-compose -p '' -f docker-compose.yml up -d --build

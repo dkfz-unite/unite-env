8c724fe18910ccea56f813cbbf19956a8f6a1c2c
vulasau <valiantsin.ulasau@gmail.com>
2023-12-21 11:51:18 +0100

    Removed redundant step.

--- applications/unite-composer/install.sh
+++ applications/unite-composer/install.sh
@@ -15,9 +15,9 @@ echo ""
 # rm -r -f src
 # echo ""
 
-tput setaf 4; echo "# Cloning fresh code to source code directory"; tput sgr0
-git clone https://$ghb_tkn@github.com/dkfz-unite/unite-composer.git src
-echo ""
+# tput setaf 4; echo "# Cloning fresh code to source code directory"; tput sgr0
+# git clone https://$ghb_tkn@github.com/dkfz-unite/unite-composer.git src
+# echo ""
 
 tput setaf 4; echo "# Building and running docker image"; tput sgr0
 # GITHUB_USER=$ghb_usr \

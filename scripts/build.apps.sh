4986dd47c584fa61b599e05cecd81addc2f19191
DGitRich <daniel.rich@gmx.net>
2024-03-01 11:59:28 +0100

    Rename install apps remote script to build apps

--- scripts/build.apps.sh
+++ scripts/build.apps.sh
@@ -1,61 +1,61 @@
 #!/bin/bash
 
 cd ../applications/unite-data-migrations
-sh install.remote.sh
+sh build.remote.sh
 cd ../../scripts
 echo ""
 echo ""
 
 cd ../applications/unite-identity-data-migrations
-sh install.remote.sh
+sh build.remote.sh
 cd ../../scripts
 echo ""
 echo ""
 
 cd ../applications/unite
-sh install.remote.sh
+sh build.remote.sh
 cd ../../scripts
 echo ""
 echo ""
 
 cd ../applications/unite-identity
-sh install.remote.sh
+sh build.remote.sh
 cd ../../scripts
 echo ""
 echo ""
 
 cd ../applications/unite-composer
-sh install.remote.sh
+sh build.remote.sh
 cd ../../scripts
 echo ""
 echo ""
 
 cd ../applications/unite-analysis
-sh install.remote.sh
+sh build.remote.sh
 cd ../../scripts
 echo ""
 echo ""
 
 cd ../applications/unite-donors-feed
-sh install.remote.sh
+sh build.remote.sh
 cd ../../scripts
 echo ""
 echo ""
 
 cd ../applications/unite-images-feed
-sh install.remote.sh
+sh build.remote.sh
 cd ../../scripts
 echo ""
 echo ""
 
 cd ../applications/unite-specimens-feed
-sh install.remote.sh
+sh build.remote.sh
 cd ../../scripts
 echo ""
 echo ""
 
 cd ../applications/unite-genome-feed
-sh install.remote.sh
+sh build.remote.sh
 cd ../../scripts
 echo ""
 echo ""

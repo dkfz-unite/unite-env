e26e2a75de211ba2a6fb085b99ee8287bde858c6
vulasau <valiantsin.ulasau@gmail.com>
2024-07-30 14:56:48 +0200

    Updated configs.

--- applications/unite-donors-feed/build.remote.sh
+++ applications/unite-donors-feed/build.remote.sh
@@ -27,4 +27,4 @@ ELASTIC_PASSWORD=$els_pwd \
 SQL_USER=$sql_usr \
 SQL_PASSWORD=$sql_pwd \
 API_KEY=$api_key \
-docker-compose -p '' -f docker-compose.build.yml up -d --build
+docker compose -p '' -f docker-compose.build.yml up -d --build

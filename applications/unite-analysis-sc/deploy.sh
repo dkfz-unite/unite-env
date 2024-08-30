7489bb3f4dbd4cf0920275641db1868534f7ad27
vulasau <valiantsin.ulasau@gmail.com>
2024-08-30 10:55:01 +0200

    Single cell RNA analysis service.

---
+++ applications/unite-analysis-sc/deploy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+tput setaf 6; echo "# Installing UNITE analysis service (Single Cell)"; tput sgr0
+echo ""
+
+tput setaf 4; echo "# Building and running docker image"; tput sgr0
+docker compose -p '' -f docker-compose.yml up -d --build

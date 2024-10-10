8c5fed5fd189c9d3348f2687cbdd9a9f66105f50
gmlavanya <lavanya.gmoorthy@gmail.com>
2024-10-10 12:53:46 +0200

    Added mongodb environment variables

--- applications/unite-donors-feed/build.remote.sh
+++ applications/unite-donors-feed/build.remote.sh
@@ -6,6 +6,8 @@ sql_usr=$(jq -r .sql.user ../../secrets.json)
 sql_pwd=$(jq -r .sql.password ../../secrets.json)
 els_usr=$(jq -r .elasticsearch.user ../../secrets.json)
 els_pwd=$(jq -r .elasticsearch.password ../../secrets.json)
+mon_usr=$(jq -r .mongodb.user ../../secrets.json)
+mon_pwd=$(jq -r .mongodb.password ../../secrets.json)
 api_key=$(jq -r .api.key ../../secrets.json)
 
 tput setaf 6; echo "# Installing UNITE Donors Datafeed service"; tput sgr0
@@ -26,5 +28,7 @@ ELASTIC_USER=$els_usr \
 ELASTIC_PASSWORD=$els_pwd \
 SQL_USER=$sql_usr \
 SQL_PASSWORD=$sql_pwd \
+MONGO_USER=$mon_usr \
+MONGO_PASSWORD=$mon_pwd \
 API_KEY=$api_key \
 docker compose -p '' -f docker-compose.build.yml up -d --build

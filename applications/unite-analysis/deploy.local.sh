32c16849487b23f8b800809988223a21006f1f76
vulasau <valiantsin.ulasau@gmail.com>
2024-11-18 15:51:51 +0100

    Local deployment scripts for analysis service.

---
+++ applications/unite-analysis/deploy.local.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+sql_usr=$(jq -r .sql.user ../../secrets.json)
+sql_pwd=$(jq -r .sql.password ../../secrets.json)
+els_usr=$(jq -r .elasticsearch.user ../../secrets.json)
+els_pwd=$(jq -r .elasticsearch.password ../../secrets.json)
+api_key=$(jq -r .api.key ../../secrets.json)
+
+tput setaf 6; echo "# Installing UNITE Analysis service"; tput sgr0
+echo ""
+
+tput setaf 4; echo "# Building and running docker image"; tput sgr0
+ELASTIC_USER=$els_usr \
+ELASTIC_PASSWORD=$els_pwd \
+SQL_USER=$sql_usr \
+SQL_PASSWORD=$sql_pwd \
+API_KEY=$api_key \
+docker compose -p '' -f docker-compose.local.yml up -d --build

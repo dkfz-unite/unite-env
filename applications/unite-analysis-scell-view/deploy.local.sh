6cc46e3a5194713385a40b2c4ed465a2b37b0c44
vulasau <valiantsin.ulasau@gmail.com>
2024-11-19 15:48:47 +0100

    Removed wrong docker pull argument.

--- applications/unite-analysis-scell-view/deploy.local.sh
+++ applications/unite-analysis-scell-view/deploy.local.sh
@@ -4,4 +4,4 @@ tput setaf 6; echo "# Installing UNITE analysis service (Single Cell Viewer)"; t
 echo ""
 
 tput setaf 4; echo "# Pulling docker image"; tput sgr0
-docker compose -p '' -f docker-compose.local.yml pull -d
+docker compose -p '' -f docker-compose.local.yml pull

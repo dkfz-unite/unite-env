fa63e4ba7df4e1aa2e4c889f7a0275b8778d5f71
vulasau <valiantsin.ulasau@gmail.com>
2026-01-22 10:24:08 +0100

    Added missing remote branch build for specimens feed.

--- applications/unite-feed-specimens/build.remote.sh
+++ applications/unite-feed-specimens/build.remote.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+branch=${1:-main}
 ghb_usr=$(jq -r .github.user ../../secrets.json)
 ghb_tkn=$(jq -r .github.token ../../secrets.json)
 sql_usr=$(jq -r .sql.user ../../secrets.json)
@@ -18,7 +19,7 @@ rm -r -f src
 echo ""
 
 tput setaf 4; echo "# Cloning fresh code to source code directory"; tput sgr0
-git clone https://$ghb_tkn@github.com/dkfz-unite/unite-feed-specimens.git src
+git clone --single-branch --depth 1 --branch "$branch" https://$ghb_tkn@github.com/dkfz-unite/unite-feed-specimens.git src
 echo ""
 
 tput setaf 4; echo "# Building and running docker image"; tput sgr0
